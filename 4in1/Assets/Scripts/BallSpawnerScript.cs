a657af763884154dc8d0ba3825acacd610129988
Sonja01 <75359906+Sonja01@users.noreply.github.com>
2023-03-27 15:25:53 +0200

    Diverse ändringar (minns ej)

---
+++ 4in1/Assets/Scripts/BallSpawnerScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawnerScript : MonoBehaviour
+{
+    [SerializeField] GameObject Ball;
+    Vector3 StartPos;
+    Quaternion StartRot;
+
+    void Start()
+    {
+        StartPos = gameObject.transform.position-new Vector3(0,1,0);
+        StartRot = gameObject.transform.rotation;
+        
+
+    }
+
+    void Update()
+    {
+
+        if (Ball.transform.position.y < -3)
+        {
+            Destroy(Ball, 2f);
+            //var ball =
+            Ball = Instantiate(Ball, StartPos, StartRot);
+        }
+    }
+}
