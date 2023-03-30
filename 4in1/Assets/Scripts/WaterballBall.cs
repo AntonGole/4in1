2e289db26093b3186af7a2e7a0008efc6d3f814c
Yogaledare <marcus_uppstrom@hotmail.com>
2023-03-30 10:55:58 +0200

    first attempt at syncing water / ball force interaction

---
+++ 4in1/Assets/Scripts/WaterballBall.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class WaterballBall : NetworkBehaviour
+{
+
+    
+    [SyncVar(hook = nameof(OnPositionChanged))]
+    private Vector3 position;
+
+    [SyncVar(hook = nameof(OnVelocityChanged))]
+    private Vector3 velocity;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnPositionChanged(Vector3 oldPosition, Vector3 newPosition)
+    {
+        rb.position = newPosition;
+    }
+
+    private void OnVelocityChanged(Vector3 oldVelocity, Vector3 newVelocity)
+    {
+        rb.velocity = newVelocity;
+    }
+
+    [Command]
+    public void CmdApplyForce(Vector3 impactForce, Vector3 impactPosition)
+    {
+        rb.AddForceAtPosition(impactForce, impactPosition, ForceMode.Impulse);
+        position = rb.position;
+        velocity = rb.velocity;
+    }
+    
+    
+    
+    
+}
---
+++ 4in1/Assets/Scripts/WaterballParticleStream.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class WaterballParticleStream : NetworkBehaviour {
+    public float forceMagnitude = 1;
+    public ParticleSystem particleSystem;
+
+    private void Start() {
+        // particleSystem = GetComponent<ParticleSystem>();
+        var collisions = particleSystem.collision;
+        collisions.enabled = true;
+        collisions.sendCollisionMessages = true;
+    }
+
+
+    private void OnParticleCollision(GameObject other) {
+        if (!hasAuthority) {
+            return;
+        }
+
+        if (other.gameObject.layer != LayerMask.NameToLayer("Ball")) {
+            return;
+        }
+
+        WaterballBall waterballBall = other.GetComponent<WaterballBall>();
+        if (waterballBall == null) {
+            return;
+        }
+
+        List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent>();
+        int numCollisionEvents = particleSystem.GetCollisionEvents(other, collisionEvents);
+        foreach (var collision in collisionEvents) {
+            var collisionVelocity = collision.velocity;
+            var collisionIntersection = collision.intersection;
+            waterballBall.CmdApplyForce(collisionVelocity * forceMagnitude, collisionIntersection);
+        }
+    }
+}
