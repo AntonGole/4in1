de9838e24227eab97122971e436091ab04f704c0
Yogaledare <marcus_uppstrom@hotmail.com>
2023-04-04 15:35:40 +0200

    ändrat kamerasystemet till orthographic + enligt ipads dimensioner

---
+++ 4in1/Assets/Scripts/WaterballCameraPositioner.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+/**
+ * Automatically position the camera based on device screen
+ */
+[ExecuteInEditMode]
+public class WaterballCameraPositioner : MonoBehaviour {
+
+
+    public GameObject floor; 
+    public static readonly float[,] PLACEMENT_RECTANGLE = { { -1f, 1f }, { 1f, 1f }, { -1f, -1f }, { 1f, -1f } };
+    public float spacing = 1f;
+    public float frustumSharing = 0;
+    public float distance = 10;
+    public float shortSideWidth = 147.84f;
+    public float shortSideHeight = 317.34f;
+    public float longSideWidth = 197.12f;
+    public float longSideHeight = 437.12f;
+    public float gapWidth = 10f;
+
+    private void Update()
+    {
+        float[,] placementArray = PLACEMENT_RECTANGLE;
+
+        int placementIndex = 0;
+        foreach (Camera child in GetComponentsInChildren<Camera>(true))
+        {
+            float x = placementArray[placementIndex, 0];
+            float y = placementArray[placementIndex, 1];
+
+            float shortSideLength = Mathf.Lerp(shortSideWidth, shortSideHeight, Mathf.Abs(y));
+            float longSideLength = Mathf.Lerp(longSideWidth, longSideHeight, Mathf.Abs(x));
+            float widthAtDistance = longSideLength * spacing;
+            float heightAtDistance = shortSideLength * spacing;
+
+            float xPos = x * (longSideLength * spacing + gapWidth);
+            float yPos = y * (shortSideLength * spacing + gapWidth);
+
+            child.transform.localRotation = Quaternion.identity;
+            child.transform.localPosition = new Vector3(xPos, yPos, 0);
+            child.lensShift = new Vector2(xPos * frustumSharing, yPos * frustumSharing);
+
+            placementIndex++;
+        }
+    }
+
+    public void setView(int viewID)
+    {
+        int placementIndex = 0;
+        foreach (Camera child in GetComponentsInChildren<Camera>(true))
+        {
+            if (placementIndex == viewID)
+            {
+                child.enabled = true;
+            }
+            else
+            {
+                child.enabled = false;
+            }
+
+
+            placementIndex++;
+        }
+    }
+}
---
+++ 4in1/Assets/Scripts/WaterballGameManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Mirror;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DefaultNamespace {
+    public class WaterballGameManager : NetworkBehaviour {
+
+
+
+        public string[] levelNames;  
+        
+
+        [SyncVar] 
+        private int currentLevel = 0;
+
+        
+
+        private void Start() {
+            levelNames =  new string[] {"GameScene", "Level 1", "Level 2" };
+        }
+
+        private void Update() {
+
+            if (SceneManager.GetActiveScene().name == "Network") {
+
+                Debug.Log("vi är i network");
+                return; 
+            }
+
+            if (SceneManager.GetActiveScene().name == "LobbyScene") {
+                Debug.Log("vi är i lobby");
+                return; 
+            }
+            
+            if (SceneManager.GetActiveScene().name == "ErrorScene") {
+                Debug.Log("vi är i error");
+                return; 
+            }
+
+
+            // Debug.Log("vi är någonstans i gamet!");
+
+
+            if (Input.GetKeyDown(KeyCode.K)) {
+                currentLevel++;
+                if (currentLevel >= levelNames.Length) {
+                    currentLevel = 0;
+                }
+
+                Debug.Log(currentLevel);
+                Debug.Log(levelNames);
+                Debug.Log(levelNames[currentLevel]);
+                GetComponent<CITENetworkManager>().ServerChangeScene(levelNames[currentLevel]);
+            }
+        }
+    }
+}
