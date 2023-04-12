fbb61b4d75e1d1bb1f98859ac50fd744db8a0be0
Yogaledare <marcus_uppstrom@hotmail.com>
2023-04-12 08:51:26 +0200

    moving gamemanager to a child of networkmanager

--- 4in1/Assets/Scripts/Waterball/WaterballGameManager.cs
+++ 4in1/Assets/Scripts/Waterball/WaterballGameManager.cs
@@ -36,6 +36,7 @@ namespace DefaultNamespace {
 
         private void Start() {
             levelNames = new string[] {"GameScene", "Level 1", "Level 2"};
+            Debug.Log("destroyar inte");
             DontDestroyOnLoad(gameObject);
         }
 
