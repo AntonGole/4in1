e25a441888be2ebc6d6dd304125fe078a74cdde9
Yogaledare <marcus_uppstrom@hotmail.com>
2023-04-18 13:31:31 +0200

    ball water physics test1

--- 4in1/Assets/Scripts/Waterball/WaterballBall.cs
+++ 4in1/Assets/Scripts/Waterball/WaterballBall.cs
@@ -1,34 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
 
-public class WaterballBall : NetworkBehaviour
-{
+public class WaterballBall : NetworkBehaviour {
 
+
+    public float dragCoefficient = 0;
+    public float rollingResistanceCoefficient = 0;
+    public float spinResistanceCoefficient = 0; 
     
-    // [SyncVar(hook = nameof(OnPositionChanged))]
-    // private Vector3 position;
-
-    // [SyncVar(hook = nameof(OnVelocityChanged))]
-    // private Vector3 velocity;
 
     private Rigidbody rb;
+    
+    
+    
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // private void OnPositionChanged(Vector3 oldPosition, Vector3 newPosition)
-    // {
-        // rb.position = newPosition;
-    // }
-
-    // private void OnVelocityChanged(Vector3 oldVelocity, Vector3 newVelocity)
-    // {
-        // rb.velocity = newVelocity;
-    // }
 
     // [Command]
     public void ApplyForce(Vector3 impactForce, Vector3 impactPosition)
@@ -37,4 +30,45 @@ public class WaterballBall : NetworkBehaviour
         // position = rb.position;
         // velocity = rb.velocity;
     }
+
+    private void FixedUpdate() {
+        Vector3 velocity = rb.velocity;
+        Vector3 angularVelocity = rb.angularVelocity;
+        float speed = velocity.magnitude;
+        float angularSpeed = angularVelocity.magnitude; 
+
+        float dragMagnitude = speed * speed * dragCoefficient;
+        float rollingResistanceMagnitude = rollingResistanceCoefficient;
+        float spinResistanceMagnitude = angularSpeed * spinResistanceCoefficient;
+        
+        Vector3 dragForce = dragMagnitude * -velocity.normalized; 
+        Vector3 rollingResistanceForce = rollingResistanceMagnitude * -velocity.normalized; 
+        Vector3 spinResistanceTorque = spinResistanceMagnitude * -angularVelocity.normalized; 
+        
+        rb.AddForce(dragForce + rollingResistanceForce);
+        rb.AddTorque(spinResistanceTorque);
+    }
 }
+
+
+    
+// [SyncVar(hook = nameof(OnPositionChanged))]
+// private Vector3 position;
+
+// [SyncVar(hook = nameof(OnVelocityChanged))]
+// private Vector3 velocity;
+
+
+
+
+// private void OnPositionChanged(Vector3 oldPosition, Vector3 newPosition)
+// {
+// rb.position = newPosition;
+// }
+
+// private void OnVelocityChanged(Vector3 oldVelocity, Vector3 newVelocity)
+// {
+// rb.velocity = newVelocity;
+// }
+
+
