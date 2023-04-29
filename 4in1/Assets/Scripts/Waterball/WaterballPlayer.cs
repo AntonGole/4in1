bfb46c84bea7bc40599aec457c57b0279d7cb0dd
Yogaledare <marcus_uppstrom@hotmail.com>
2023-04-29 14:19:59 +0200

    dsa

--- 4in1/Assets/Scripts/Waterball/WaterballPlayer.cs
+++ 4in1/Assets/Scripts/Waterball/WaterballPlayer.cs
@@ -42,7 +42,8 @@ public class WaterballPlayer : CITEPlayer {
 
         isRotating = false;
         // WaterballCanvasManager.Instance.LoadSceneUI(playerID);
-        playerHUD.GetComponent<WaterballPlayerHUD>().LoadSceneUI();
+        
+        // playerHUD.GetComponent<WaterballPlayerHUD>().LoadSceneUI();
         var scene = SceneManager.GetActiveScene().name;
         if (scene == "Title Screen") {
             barrelPart.SetActive(false);
--- 4in1/Assets/Scripts/Waterball/WaterballReadyButtonMessage.cs
+++ 4in1/Assets/Scripts/Waterball/WaterballReadyButtonMessage.cs
@@ -1,21 +1,21 @@
-﻿using Mirror;
-using UnityEngine;
-
-public struct WaterballReadyButtonMessage : NetworkMessage {
-    
-    public int playerID;
-    public bool IsReady;
-
-    
-    
-    
-    // public WaterballReadyButtonMessage() { }
-    //
-    // public WaterballReadyButtonMessage(int playerID, bool IsReady)
-    // {
-    //     this.playerID = playerID;
-    //     this.IsReady = IsReady;
-    // }
-    
-}
-
+﻿// using Mirror;
+// using UnityEngine;
+//
+// public struct WaterballReadyButtonMessage : NetworkMessage {
+//     
+//     public int playerID;
+//     public bool IsReady;
+//
+//     
+//     
+//     
+//     // public WaterballReadyButtonMessage() { }
+//     //
+//     // public WaterballReadyButtonMessage(int playerID, bool IsReady)
+//     // {
+//     //     this.playerID = playerID;
+//     //     this.IsReady = IsReady;
+//     // }
+//     
+// }
+//
