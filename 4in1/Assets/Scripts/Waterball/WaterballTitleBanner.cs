8aa5d1e833c1e6d2eeea4f91d8c1ee2005a902aa
Yogaledare <marcus_uppstrom@hotmail.com>
2023-05-02 17:57:32 +0200

    testar network transform för title banner

--- 4in1/Assets/Scripts/Waterball/WaterballTitleBanner.cs
+++ 4in1/Assets/Scripts/Waterball/WaterballTitleBanner.cs
@@ -14,32 +14,15 @@ public class WaterballTitleBanner : NetworkBehaviour {
 
     public float circularBannerRotationSpeed = 30f;
 
-    private float lastRotation;
-    private float lerpSpeed = 1f; 
-    
-    [SyncVar]
-    public float syncedRotation;
+
 
     private void Start() {
-        if (isServer) {
-            // If this is the server, set the initial rotation value
-            syncedRotation = transform.rotation.eulerAngles.z;
-        }
 
-        lastRotation = syncedRotation;
     }
 
     private void Update() {
         if (!circularBanner) {
             return;
-        }
-
-        if (isServer) {
-            syncedRotation += circularBannerRotationSpeed * Time.deltaTime;
-            // syncedRotation = syncedRotation % 360f; 
-        }
-        else {
-            lastRotation = Mathf.Lerp(lastRotation, syncedRotation, Time.deltaTime * lerpSpeed);
         }
 
         RotateBanner();
@@ -53,8 +36,7 @@ public class WaterballTitleBanner : NetworkBehaviour {
 
 
     private void RotateBanner() {
-        circularBanner.transform.localRotation = Quaternion.Euler(0, 0, isServer ? syncedRotation : lastRotation);
-        // circularBanner.transform.Rotate(new Vector3(0, 0, -circularBannerRotationSpeed * Time.deltaTime));
+        circularBanner.transform.Rotate(new Vector3(0, 0, -circularBannerRotationSpeed * Time.deltaTime));
     }
 
 
@@ -62,27 +44,3 @@ public class WaterballTitleBanner : NetworkBehaviour {
 
 
 }
-
-
-// private IEnumerator ShowCountdown(float displayTime) {
-//     countdownNumbers.SetActive(false);
-//     yield return new WaitForSeconds(displayTime);
-//     countdownNumbers.SetActive(true);
-//     foreach (var numberSprite in numberSprites) {
-//         foreach (var numberHolder in numberHolders) {
-//             Image numberImage = numberHolder.transform.GetChild(0).GetComponent<Image>();
-//             numberImage.sprite = numberSprite;
-//         }
-//
-//         yield return new WaitForSeconds(displayTime);
-//     }
-//
-//     countdownNumbers.SetActive(false);
-// }
-
-
-// private IEnumerator ShowCircularBanner(float displayTime) {
-// circularBanner.SetActive(true);
-// yield return new WaitForSeconds(displayTime); 
-// circularBanner.SetActive(false);
-// }
