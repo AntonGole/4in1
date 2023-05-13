2a7394d024aad41cc91c4da8e87a2043b3d3c572
Sonja01 <75359906+Sonja01@users.noreply.github.com>
2023-05-13 15:08:24 +0200

    change order of levels (forgot to push)

--- 4in1/Assets/Scripts/Waterball/WaterballGameManager.cs
+++ 4in1/Assets/Scripts/Waterball/WaterballGameManager.cs
@@ -75,13 +75,11 @@ public class WaterballGameManager : NetworkBehaviour {
         levelNames = new List<string>();
         // levelNames.Add("GameScene");
         levelNames.Add("Title Screen");
-        levelNames.Add("Level 8");
         // levelNames.Add("Level 16");
-        levelNames.Add("Level 9");
         levelNames.Add("Level 18");
-        
-        levelNames.Add("Level 10");
+        levelNames.Add("Level 8");
         levelNames.Add("Level 13");
+        levelNames.Add("Level 9");
         levelNames.Add("Level 19");
         levelNames.Add("Ending Screen");
         
